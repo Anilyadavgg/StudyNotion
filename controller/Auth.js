75e32148bfc0095e04f385a1c4f3a5617078e88f
anilyadavgg <anilyadavg245@gmail.com>
2025-07-17 21:29:11 +0530

    push code

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const OTP = require("../models/OTP");
 const otpGenerator = require("otp-generator");
+const { response } = require("express");
 
 // send otp
 exports.sendOTP = async (req,res) =>{
@@ -62,6 +63,82 @@ exports.sendOTP = async (req,res) =>{
 };
 
 // signup
+exports.signup = async(req,res)=>{
+    // fetch data from req.body
+    const {
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPassword,
+        accountType,
+        contactNumber,
+        otp,
+    } = req.body;
+    // validate data
+    if(!firstName || !lastName || !email || !password || !confirmPassword || !otp){
+        return res.status(403).json({
+            success:false,
+            message:'All field are required',
+        })
+    }
+    // match password
+    if(password !== confirmPassword){
+        return res.status(403).json({
+            success:false,
+            message:'Password and ConfirmPassword value does not match, please try again',
+        })
+    }
+    // check user already present or not
+    const exixtingUser = await User.findOne({email});
+    if(exixtingUser){
+        return res.status(403).json({
+            success:false,
+            message:'User is already registered',
+        })
+    }
+
+    // find most recent otp stored for the user
+    const recentOtp = await OTP.find({email}).sort({createAt:-1}).limit(1);
+    console.log(recentOtp);
+    // validate otp
+    if(recentOtp ,length == 0){
+        return res.status(403).json({
+            success:false,
+            message:'OTP not Found',
+        })
+    }else if(otp !== recentOtp){
+        // invalid otp
+        return res.status(403).json({
+            success:false,
+            message:'Invalid OTP'
+        })
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password,10);
+    // entry create in db
+
+    const profileDetails = await Profile.create({
+        gender:null,
+        dateOfBirth:null,
+        about:null,
+        contactNumber:null,
+    })
+    const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        contactNumber,
+        password:hashedPassword,
+        accountType,
+        accountDetails:
+    })
+
+    // return res
+
+
+}
 
 // login
 
