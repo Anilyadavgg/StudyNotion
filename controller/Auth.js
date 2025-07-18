2877ba8c96691cbc217062157c9b2869ba9e6c55
anilyadavgg <anilyadavg245@gmail.com>
2025-07-18 16:40:48 +0530

    add code

--- controller/Auth.js
+++ controller/Auth.js
@@ -2,6 +2,9 @@ const User = require("../models/user");
 const OTP = require("../models/OTP");
 const otpGenerator = require("otp-generator");
 const { response } = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // send otp
 exports.sendOTP = async (req,res) =>{
@@ -64,82 +67,180 @@ exports.sendOTP = async (req,res) =>{
 
 // signup
 exports.signup = async(req,res)=>{
-    // fetch data from req.body
-    const {
-        firstName,
-        lastName,
-        email,
-        password,
-        confirmPassword,
-        accountType,
-        contactNumber,
-        otp,
-    } = req.body;
-    // validate data
-    if(!firstName || !lastName || !email || !password || !confirmPassword || !otp){
-        return res.status(403).json({
-            success:false,
-            message:'All field are required',
+    try{
+        // fetch data from req.body
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+            accountType,
+            contactNumber,
+            otp,
+        } = req.body;
+        // validate data
+        if(!firstName || !lastName || !email || !password || !confirmPassword || !otp){
+            return res.status(403).json({
+                success:false,
+                message:'All field are required',
+            })
+        }
+        // match password
+        if(password !== confirmPassword){
+            return res.status(403).json({
+                success:false,
+                message:'Password and ConfirmPassword value does not match, please try again',
+            })
+        }
+        // check user already present or not
+        const exixtingUser = await User.findOne({email});
+        if(exixtingUser){
+            return res.status(403).json({
+                success:false,
+                message:'User is already registered',
+            })
+        }
+
+        // find most recent otp stored for the user
+        const recentOtp = await OTP.find({email}).sort({createAt:-1}).limit(1);
+        console.log(recentOtp);
+        // validate otp
+        if(recentOtp ,length == 0){
+            return res.status(403).json({
+                success:false,
+                message:'OTP not Found',
+            })
+        }else if(otp !== recentOtp){
+            // invalid otp
+            return res.status(403).json({
+                success:false,
+                message:'Invalid OTP'
+            })
+        }
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(password,10);
+        // entry create in db
+
+        const profileDetails = await Profile.create({
+            gender:null,
+            dateOfBirth:null,
+            about:null,
+            contactNumber:null,
+        })
+        const user = await User.create({
+            firstName,
+            lastName,
+            email,
+            contactNumber,
+            password:hashedPassword,
+            accountType,
+            accountDetails:profileDetails._id,
+            image:`https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`,
+        })
+        // return res
+        return res.status(200).json({
+            success:true,
+            message:'User is registered successfully',
+            user,
         })
     }
-    // match password
-    if(password !== confirmPassword){
-        return res.status(403).json({
+    catch(error){
+        console.log(error);
+        return res.status(500).jsom({
             success:false,
-            message:'Password and ConfirmPassword value does not match, please try again',
+            message:'User cannot be regisred. Please try again',
         })
     }
-    // check user already present or not
-    const exixtingUser = await User.findOne({email});
-    if(exixtingUser){
-        return res.status(403).json({
-            success:false,
-            message:'User is already registered',
-        })
-    }
-
-    // find most recent otp stored for the user
-    const recentOtp = await OTP.find({email}).sort({createAt:-1}).limit(1);
-    console.log(recentOtp);
-    // validate otp
-    if(recentOtp ,length == 0){
-        return res.status(403).json({
-            success:false,
-            message:'OTP not Found',
-        })
-    }else if(otp !== recentOtp){
-        // invalid otp
-        return res.status(403).json({
-            success:false,
-            message:'Invalid OTP'
-        })
-    }
-
-    // hash password
-    const hashedPassword = await bcrypt.hash(password,10);
-    // entry create in db
-
-    const profileDetails = await Profile.create({
-        gender:null,
-        dateOfBirth:null,
-        about:null,
-        contactNumber:null,
-    })
-    const user = await User.create({
-        firstName,
-        lastName,
-        email,
-        contactNumber,
-        password:hashedPassword,
-        accountType,
-        accountDetails:
-    })
-
-    // return res
 
 
 }
 
 // login
+exports.login = async (req, res)=>{
+    try{
+        // get data from req body
+        const {email,password} = req.body;
+         // validation data
+        if(!email || !password){
+            return res.status(403).json({
+                success:false,
+                message:'All fields are required , please try again',
+            });
+        }
+
+        // user check exist or not
+        const user = await User.findOne({email}).populate("aditionalDetails");
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:'User is not registered , please try again',
+            });
+        }
+
+        // generate JWT token, after password match
+        if(await bcrypt.compare(password, user.password)){
+            const payload = {
+                email:user.email,
+                id:user._id,
+                role:user.role,
+            }
+            const token = jwt.sign(payload, process.env.JWT_SECRET,{
+                expiresIn:"2h",
+            });
+            user.token = token;
+            user.password = undefined;
+        
+
+            // create cookie and send response
+            const options = {
+                expires:new Date(Date.now()+3*24*60*60*1000),
+                httpOnly:true,
+            }
+            res.cookie("token",token,options).status(200).json({
+                success:false,
+                token,
+                user,
+                message:'Logged in successfully',
+            })
+        }
+        else{
+            return res.status(401).json({
+                success:false,
+                message:'Password is  Incorrect',
+            });
+        }
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:'Login Failure, please try again',
+        }) ;
+    }
+};
+
+
 
 // changePassword
+// TODO :HomeWork
+exports.changePassword = async(req,res)=>{
+    try{
+        // get data from req body
+
+        // get old password , new password , confirm new password
+        
+        // validation
+
+        // update pwd in db
+
+        // send mail - password updated
+
+        // return response
+
+    }
+    catch(error){
+
+    }
+}
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,50 @@
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const User = require("../models/user");
+const { startSucceeded } = require('init');
+
+
 // auth 
+exports.auth = async(req,res,next) =>{
+    try{
+        // extract token
+        const token = req.cookies.token
+                         ||req.body.token 
+                         || req.header("Authorisation").replace("Bearer ","");
+        // token missing, then return response
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                message:'Token is missing',
+            });
+        }
+        // verify the token 
+        try{
+            const decode = await jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decode);
+            req.user = decode;
+        }
+        catch(error){
+            return res.status(4001).json({
+                success:false,
+                message:'token is invalid',
+            });
+
+        }
+        next();
+    }
+    catch(error){
+        return res.status(4001).json({
+            success:false,
+            message:'Something went wrong while  validating the token',
+        });
+    }
+}
 
 // isStudent
+exports.isStudent = async(req,res,next) =>{
+    
+}
 
 // isInstructor
 
